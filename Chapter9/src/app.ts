0afcad4c1e1c81ec934582cc8f01b0ac2aa87e14
Youjiiin <yujinjang24@gmail.com>
2024-04-29 16:01:11 +0900

    Chapter9: input 요소 접근

--- Chapter9/src/app.ts
+++ Chapter9/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
@@ -12,7 +15,25 @@ class ProjectInput {
         // 위는 생성자 안에 있고, DocumentFragment이기 때문에 삽입할 수 없다.(insertAdjacentElement)에
         // HTMLFormElement가 될 것이라고 알려줘야 한다. 
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        // 요소에 접근
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        // this가 이벤트의 현재 타깃에 바인딩 되기 때문에 vlaue값을 가져오지 못한다.
+        console.log(this.titleInputElement.value); 
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attach() {
