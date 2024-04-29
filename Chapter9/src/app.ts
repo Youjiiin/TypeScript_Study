452c36fce1e64b1566affe408bf056d1005801c2
Youjiiin <yujinjang24@gmail.com>
2024-04-29 17:43:46 +0900

    Chapter9: 리스트 렌더링

--- Chapter9/src/app.ts
+++ Chapter9/src/app.ts
@@ -60,6 +60,34 @@ function autobind(
     return adjDescriptor;
 }
 
+//ProjectList 클래스
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-list')!;
+        this.hostElement = <HTMLDivElement>document.getElementById('app')!;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-project-list`;
+        this.element .querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+
 // 프로젝트 input 클래스
 class ProjectInput {
     templateElement: HTMLTemplateElement;
@@ -155,3 +183,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
